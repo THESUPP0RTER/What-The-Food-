6ac4a668bad7263b78343723aa8b7ba46569c03e
Matthew R Quinn <MatthewRileyQuinn@gmail.com>
2021-12-10 16:50:30 -0800

    Work in progress getting MariaDB to connect using  Pomelo.EntityFrameworkCore.MySql

--- Workspace/Console Runner/Console Runner/Program.cs
+++ Workspace/Console Runner/Console Runner/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using User;
+
 Console.WriteLine("Hello, World!");
-var connectionString = "Server = myServerAddress; Database = myDataBase; Uid = myUsername; Pwd = myPassword;";
+var connectionString = "Server -= (localdb); Database = myDataBase; Uid = root; Pwd = myPassword;";
+
+Customer test = new Customer();
+test.name = "Matt";
+test.email = "testttt";
+Startup letsGo = new Startup();
+letsGo.ConfigureServices();
+letsGo.save
---
+++ Workspace/Console Runner/User/Customer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace User
+{
+    public class Customer
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+
+    }
+}
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // Replace with your connection string.
+        var connectionString = "server=localhost;user=root;password=myPassword;database=ef";
+
+        // Replace with your server version and type.
+        // Use 'MariaDbServerVersion' for MariaDB.
+        // Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
+        // For common usages, see pull request #1233.
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
+
+        // Replace 'YourDbContext' with the name of your own DbContext derived class.
+        services.AddDbContext<DbContext>(
+             dbContextOptions => dbContextOptions
+                 .UseMySql(connectionString, serverVersion)
+                 // The following three options help with debugging, but should
+                 // be changed or removed for production.
+                 .LogTo(Console.WriteLine, LogLevel.Information)
+                 .EnableSensitiveDataLogging()
+                 .EnableDetailedErrors()
+         );
+    }
+}
